439d569a493c312db14cbf55c895bbd85a0fa10f
Junsha111 <shahjunaid999@gmail.com>
2024-05-11 18:17:37 +0530

    add conditional

---
+++ script2.js
@@ -0,0 +1,11 @@
+let age=-1
+if(age >=18){
+    console.log("u can drive")
+    
+}
+else if(age <18 && age>0){
+    console.log("u cannot drive")
+}
+else if(age<=0){
+    console.log("invalid age")
+}
